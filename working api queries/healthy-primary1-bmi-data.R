6f190fe72a3d33b838ec2e181d57a01686c8d61c
Mairi-Louise MacDonald <mairi_louise@yahoo.co.uk>
2022-07-27 15:11:54 +0100

    Update dataset formats to facilitate a join to create masterdata

---
+++ working api queries/healthy-primary1-bmi-data.R
@@ -0,0 +1,27 @@
+library(httr)
+library(dplyr)
+library(phsopendata)
+library(tidyr)
+
+res_id <- "e9f8d10c-9c06-4e77-a0f5-70ff14af25a4"
+BMI_dataset <- phsopendata::get_resource(res_id = res_id)
+healthy_BMI <- BMI_dataset[c("SchoolYear", "CA", "EpiHealthyWeight")]
+
+rolled_avg_BMI <- healthy_BMI %>%
+  dplyr::arrange(desc(CA)) %>% 
+  dplyr::group_by(CA) %>% 
+  dplyr::mutate("threeYrAvg" = 100*(zoo::rollmean(EpiHealthyWeight, k = 3, fill = NA))) %>% 
+  dplyr::ungroup() %>%
+  drop_na("threeYrAvg")
+
+code_lookup_table <- read.csv("code_lookup.csv") %>%
+  select(CA, CPP) %>%
+  distinct()
+
+p1_bmi_data <- left_join(rolled_avg_BMI, code_lookup_table, by = "CA") %>%
+  arrange(CPP) %>%
+  select(CPP, SchoolYear, threeYrAvg) %>%
+  rename("Year" = SchoolYear, "value" = threeYrAvg) %>%
+  mutate(Indicator = "Primary 1 Body Mass Index", Type = "Raw")
+
+write.csv(p1_bmi_data, file = "Final Data/p1_bmi_data.csv")
