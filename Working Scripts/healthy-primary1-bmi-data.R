a46f0dbbfc84c0d686028d48861b5942fd4bee58
Mairi-Louise MacDonald <mairi_louise@yahoo.co.uk>
2022-07-27 15:12:55 +0100

    more reformatting

--- Working Scripts/healthy-primary1-bmi-data.R
+++
@@ -1,8 +0,0 @@
-library(httr)
-library(dplyr)
-library(phsopendata)
-
-res_id <- "e9f8d10c-9c06-4e77-a0f5-70ff14af25a4"
-BMI_dataset <- phsopendata::get_resource(res_id = res_id)
-healthy_BMI <- BMI_dataset[c("SchoolYear", "CA", "EpiHealthyWeight")]
-
